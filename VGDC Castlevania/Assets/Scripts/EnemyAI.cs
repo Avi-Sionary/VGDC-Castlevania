14280b818610ab2fb2d72eb3fe9334d9ce25a790
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2016-11-13 22:17:25 -0800

    My scene and code for enemy AI
    If there is anything wrong with it or can be done better please tell me.

---
+++ VGDC Castlevania/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+    public BoxCollider2D enemyboundry;
+    GameObject Player;
+
+    EnemyActions enemy;
+
+    void start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        enemy = enemy.GetComponent<EnemyActions>();
+    }
+    void Update()
+    {
+        enemy.update();
+        enemy.EnemyAttack();
+    }
+    void OnTriggerStay(BoxCollider2D other)
+    {
+        enemy.TransformEnemy();
+    }
+}
+
+public class EnemyActions : MonoBehaviour
+{
+    public Transform character;
+    public int speed = 3;
+    public int EnemyAttackRange = 1;
+    public int EnemyAttackTime = 5;
+    public int EnemyHealthPoints = 20;
+
+    GameObject Player;
+
+    public void start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public void update()
+    {
+        if(EnemyAttackTime > 0)
+        {
+            EnemyAttackTime -= 1;
+        }
+        if(EnemyAttackTime == 0)
+        {
+            EnemyAttack();
+            EnemyAttackTime = 5; /*Cooldown Time for next attack*/
+        }
+    }
+    public void EnemyDeath(int playerdamage)
+    {
+        Debug.Log("Enemy took damage");
+
+        EnemyHealthPoints -= playerdamage;
+
+        if (EnemyHealthPoints <= 0) /*If the enemy dies from player*/
+        {
+            Debug.Log("EnemyDied");
+
+            Destroy(gameObject);
+            Destroy(this);
+        }
+    }
+    public void TransformEnemy()  /*Enemy Movement*/
+    {
+        transform.LookAt(character.position);
+
+        if (Vector2.Distance(transform.position, character.position) > EnemyAttackRange)
+        {
+            transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+        }
+    }
+    public void EnemyAttack()
+    {
+        if(Vector2.Distance(transform.position, character.position) <= EnemyAttackRange)
+        {
+            Player.SendMessage("TakeDamage", 1); /*The Amount of Damage dealt by the enemy*/
+        }
+    }
+}
